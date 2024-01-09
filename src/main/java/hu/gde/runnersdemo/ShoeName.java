e42f709ce1b7b8b7fdb92dce54f59748a876a0bb
Mark <obimaark@gmail.com>
2024-01-09 18:19:23 +0100

    Új ShoeName Class + Repository

---
+++ src/main/java/hu/gde/runnersdemo/ShoeName.java
@@ -0,0 +1,35 @@
+package hu.gde.runnersdemo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ShoeName {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long shoeId;
+    private String shoeName;
+
+    @ManyToOne
+    @JoinColumn(name = "runner_id")
+    private RunnerEntity runner;
+
+    public long getShoeId() {
+        return shoeId;
+    }
+
+    public String getShoeName() {
+        return shoeName;
+    }
+    public void setRunner(RunnerEntity runner) {
+        this.runner = runner;
+    }
+
+    public void setShoeId(long shoeId) {
+        this.shoeId = shoeId;
+    }
+
+    public void setShoeName(String shoeName) {
+        this.shoeName = shoeName;
+    }
+}
